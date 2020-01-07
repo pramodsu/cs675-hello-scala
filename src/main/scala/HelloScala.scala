af6d7568a3196e9e9b9f50dc69d4e90e16b6ac07
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2020-01-07 15:49:59 +0530

    Files updated after class.

--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -6,7 +6,7 @@ case class Var(name: String) extends Expr {
   override def toString() = name
 }
 case class And(e1: Expr, e2: Expr) extends Expr {
-  override def toString() = e1.toString() + " && " + e2.toString()
+  override def toString() = "(" + e1.toString() + " && " + e2.toString() + ")"
 }
 case class Not(e: Expr) extends Expr {
   override def toString() = "!" + e.toString()
@@ -25,19 +25,34 @@ case class CallStmt(f: String) extends Stmt {
 object HelloScala {
   def findFunctions(prog : Stmt) : Set[String] = {
     prog match {
-      case IfElseStmt(e, t, f) => findFunctions(t) ++ findFunctions(f)
       case CallStmt(f) => Set(f)
+      case IfElseStmt(e, t, f) =>
+        findFunctions(t) ++ findFunctions(f)
+    }
+  }
+  def findVariablesInExpr(e : Expr) : Set[String] = {
+    e match {
+      case Var(n) => Set(n)
+      case And(e1, e2) => findVariablesInExpr(e1) ++ findVariablesInExpr(e2)
+      case Not(e) => findVariablesInExpr(e)
+    }
+  }
+
+  def findVariables(prog: Stmt) : Set[String] = {
+    prog match {
+      case CallStmt(f) => Set.empty
+      case IfElseStmt(e, t, f) =>
+        findVariablesInExpr(e) ++ findVariables(t) ++ findVariables(f)
     }
   }
 
   def main(args: Array[String]) {
     printf("hello, world!\n")
-    val c1 = And(Not(Var("a")), Not(Var("b")))
-    val c2 = Not(Var("a"))
-    val s1 = IfElseStmt(c1, 
+    val s1 = IfElseStmt(And(Not(Var("a")), Not(Var("b"))), 
                 CallStmt("h"), 
-                IfElseStmt(c2, CallStmt("g"), CallStmt("f")))
+                IfElseStmt(Not(Var("a")), CallStmt("g"), CallStmt("f")))
     println(s1.toString())
     println("Functions in the above: " + findFunctions(s1))
+    println("Variables in the above: " + findVariables(s1))
   }
 }
--- src/test/scala/HelloTest.scala
+++ src/test/scala/HelloTest.scala
@@ -2,10 +2,17 @@ package hello
 package test
 
 import org.scalatest.FlatSpec
-import hello.code.HelloScala
+import hello.code._
 
 class ParserSpec extends FlatSpec {
   "main" should "run" in {
     HelloScala.main(Array("HelloScala"))
+  }
+  "first test case for findFunctions" should "work correctly" in {
+    val s1 = IfElseStmt(And(Not(Var("a")), Not(Var("b"))), 
+                CallStmt("h"), 
+                IfElseStmt(Not(Var("a")), CallStmt("g"), CallStmt("f")))
+    var fns = HelloScala.findFunctions(s1)
+    assert(fns == Set("h", "g", "f"))
   }
 }
